d27d448d2f21fa52de058a2dcb5cea551f66df22
iPenguwin <220711663@students.uajy.ac.id>
2025-10-31 15:19:27 +0700

    sdkappid and sdsecretkey

--- TUIRoomKit/Web/example/webpack-vue2.7-ts/src/config/basic-info-config.js
+++ TUIRoomKit/Web/example/webpack-vue2.7-ts/src/config/basic-info-config.js
@@ -2,7 +2,7 @@
  * @Description: Basic information configuration for TUIRoomKit applications
  */
 
-import LibGenerateTestUserSig from './lib-generate-test-usersig-es.min';
+import LibGenerateTestUserSig from "./lib-generate-test-usersig-es.min";
 
 /**
  * Tencent Cloud SDKAppId, which should be replaced with user's SDKAppId.
@@ -11,7 +11,7 @@ import LibGenerateTestUserSig from './lib-generate-test-usersig-es.min';
  * It is a unique identifier used by Tencent Cloud to identify users.
  */
 
-export const SDKAPPID = 0;
+export const SDKAPPID = 20028842;
 
 /**
  * Encryption key for calculating signature, which can be obtained in the following steps:
@@ -28,7 +28,8 @@ export const SDKAPPID = 0;
  * Document: https://intl.cloud.tencent.com/document/product/647/35166#Server
  *
  */
-export const SDKSECRETKEY = '';
+export const SDKSECRETKEY =
+    "22c728b592736d111360d9d886421b2a2247d14abe403d0fc914886fba040b6d";
 
 /**
  * Signature expiration time, which should not be too short
@@ -42,27 +43,31 @@ export const EXPIRETIME = 604800;
  * Set user information on the push side
  */
 export const userInfo = {
-  // userId
-  userId: `user_${Math.ceil(Math.random() * 100000)}`,
-  // userName
-  userName: 'myName',
-  // userAvatar
-  avatarUrl: '',
+    // userId
+    userId: `user_${Math.ceil(Math.random() * 100000)}`,
+    // userName
+    userName: "myName",
+    // userAvatar
+    avatarUrl: "",
 };
 
 export function getBasicInfo() {
-  if (SDKAPPID === Number(0) || SDKSECRETKEY === String('')) {
-    alert('Please configure your SDKAPPID in config/basic-info-config.js');
-    return;
-  }
-  const generator = new LibGenerateTestUserSig(SDKAPPID, SDKSECRETKEY, EXPIRETIME);
-  const userSig = generator.genTestUserSig(userInfo.userId);
-  const { userId, userName, avatarUrl } = userInfo;
-  return {
-    sdkAppId: SDKAPPID,
-    userId,
-    userSig,
-    userName,
-    avatarUrl,
-  };
-};
+    if (SDKAPPID === Number(0) || SDKSECRETKEY === String("")) {
+        alert("Please configure your SDKAPPID in config/basic-info-config.js");
+        return;
+    }
+    const generator = new LibGenerateTestUserSig(
+        SDKAPPID,
+        SDKSECRETKEY,
+        EXPIRETIME
+    );
+    const userSig = generator.genTestUserSig(userInfo.userId);
+    const { userId, userName, avatarUrl } = userInfo;
+    return {
+        sdkAppId: SDKAPPID,
+        userId,
+        userSig,
+        userName,
+        avatarUrl,
+    };
+}
